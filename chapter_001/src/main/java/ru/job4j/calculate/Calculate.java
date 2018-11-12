4dc3b726d7d361a5acc4846e7d18f556f82934f2
kejam <kejamwork@mail.ru>
2018-11-12 16:57:08 +0300

    .git1.4. JUnit[#85061]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -13,6 +13,15 @@ package ru.job4j.calculate;
 * @param args - args
 */
     public static void main(String[] args){
-	    System.out.println("Hello job4j!!!");
+	    System.out.println("Hello world");
     }
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+    public String echo(String name) {
+           return "Echo, echo, echo : " + name;
+    }
+  
 }
