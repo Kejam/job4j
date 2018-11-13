a45133a527f48ea732bbe9f81fdc3ec259dc8f54
kejam <kejamwork@mail.ru>
2018-11-13 14:26:00 +0300

    test

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,27 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-*  Calculate решение задачи
-*  @author Aleksey Prozorov (kejamwork@mail.ru)
-*  @since 12.11.2018
-*  @version 1
-*/
- public class Calculate {
-	
-/**
-* Конструктор, вывод строки в консоль
-* @param args - args
-*/
+ *  Calculate решение задачи
+ *  @author Aleksey Prozorov (kejamwork@mail.ru)
+ *  @since 12.11.2018
+ *  @version 1
+ */
+public class Calculate {
+
+    /**
+     * Конструктор, вывод строки в консоль
+     * @param args - args
+     */
     public static void main(String[] args) {
-	    System.out.println("Hello world");
+        System.out.println("Hello world ываываыва");
     }
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
     public String echo(String name) {
-           return "Echo, echo, echo : " + name;
+        return "Echo, echo, echo : " + name;
     }
-  
+
 }
