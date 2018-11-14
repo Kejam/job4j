b729e7ae1d03ecf872e50cc01f477ad6f2215e28
kejam <kejamwork@mail.ru>
2018-11-14 19:29:45 +0300

    85052

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+
+/**
+ * @author Prozorov Aleksey (kejamwork@mail.ru).
+ * @version $Id$
+ * @since 0.1
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    /**
+     * Конструктор класса с x и y.
+     * @param x - x.
+     * @param y - y.
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * Method echo.
+     * @param that Point.
+     * @return distance.
+     */
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+    /**
+     * Конструктор, вывод строки в консоль.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+
+
+}
