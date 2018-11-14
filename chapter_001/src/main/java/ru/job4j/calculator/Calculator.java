4544acfa8f4e45b44d33e32d1e01d89d124a97c9
kejam <kejamwork@mail.ru>
2018-11-14 17:57:24 +0300

    123

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,46 @@
 package ru.job4j.calculator;
+/*
+* Калькулятор с простыми действиями.
+*/
 
 public class Calculator {
     private double result;
-
+    /**
+     * Method add.
+     * @param first first param.
+     * @param second second param .
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
-
+    /**
+     * Method substract.
+     * @param first first param.
+     * @param second second param .
+     */
     public void substract(double first, double second) {
         this.result = first - second;
     }
-
+    /**
+     * Method div.
+     * @param first first param.
+     * @param second second param .
+     */
     public void div(double first, double second) {
         this.result = first / second;
     }
-
+    /**
+     * Method multiple.
+     * @param first first param.
+     * @param second second param .
+     */
     public void multiple(double first, double second) {
         this.result = first * second;
     }
-
+    /**
+     * Method getResult.
+     * @return result.
+     */
     public double getResult() {
         return this.result;
     }
