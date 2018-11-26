c72162fc3cf9bf6385af24b9989de6bc4deb9347
Aleksey Prozorov <kejamwork@mail.ru>
2018-11-26 15:26:52 +0300

    test

---
+++ chapter_002/src/main/java/ru/job4j/chess/Board.java
@@ -0,0 +1,25 @@
+package ru.job4j.chess;
+
+
+import ru.job4j.chess.firuges.*;
+
+public class Board extends Figure{
+    Figure[] figures = new Figure[32];
+    int position = 0;
+    public void add(Figure figure) {
+        figures[position++] = figure;
+    }
+    boolean move(Cell source, Cell dest) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException {
+        if (!figures.) {
+
+        }
+        if (...) {
+
+        }
+        if (...) {
+
+        }
+        Figure figure.
+        return true;
+    }
+}
