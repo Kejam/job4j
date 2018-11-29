e3d1bf410c2d97f6e6b7ee0c2c596e01cff1be1d
Aleksey Prozorov <kejamwork@mail.ru>
2018-11-29 17:28:43 +0300

    temp

---
+++ chapter_003/src/main/java/ru/job4j/bank/Account.java
@@ -0,0 +1,31 @@
+package ru.job4j.bank;
+
+import java.util.Objects;
+
+public class Account {
+    private int value;
+    private int requisites;
+    public Account(int value, int requisites) {
+        this.value = value;
+        this.requisites = requisites;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Account account = (Account) o;
+        return value == account.value && requisites == account.requisites;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, requisites);
+    }
+    @Override
+    public String toString() {
+        return "Account{" + "value=" + value + ", requisites=" + requisites + '}';
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -0,0 +1,48 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class Bank {
+    private Map<User, List<Account>> bankAccounts = new HashMap<>();
+    public void addUser(User user) {
+        this.bankAccounts.putIfAbsent(user, new ArrayList<>());
+    }
+    public void deleteUser(User user) {
+        if (bankAccounts.containsKey(user)) {
+            bankAccounts.remove(user);
+        }
+    }
+    public void addAccountToUser(String passport, Account account) {
+        User user = findUser(passport);
+        if (bankAccounts.containsKey(user)) {
+            bankAccounts.get(user).add(account);
+        }
+    }
+    public void deleteAccountFromUser(String passport, Account account) {
+        User user = findUser(passport);
+        if (bankAccounts.containsKey(user)) {
+            bankAccounts.get(user).remove(account);
+        }
+    }
+    private User findUser(String passport) {
+        for (Map.Entry<User, List<Account>> desired : this.bankAccounts.entrySet()) {
+            if (desired.getKey().getPassport().equals(passport)) {
+                return desired.getKey();
+            }
+        }
+        return null;
+    }
+    public List<Account> getUserAccounts (String passport) {
+        User user = findUser(passport);
+        List<Account> accounts =  bankAccounts.get(user);
+        return accounts;
+    }
+    public boolean transferMoney (String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount) {
+        boolean result = false;
+
+        return result;
+    }
+    private Account getActualAccount(String passport, String requisite) {
+
+    }
+}
