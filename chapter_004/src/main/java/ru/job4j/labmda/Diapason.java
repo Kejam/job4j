b0b1ed08c0cc6c8699109861e6053dfb2e06175b
Aleksey Prozorov <kejamwork@mail.ru>
2018-12-03 18:11:18 +0300

    test

---
+++ chapter_004/src/main/java/ru/job4j/labmda/Diapason.java
@@ -0,0 +1,16 @@
+package ru.job4j.labmda;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Diapason {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new LinkedList<>();
+        while (start != end) {
+            
+            start++;
+        }
+        return result;
+    }
+}
