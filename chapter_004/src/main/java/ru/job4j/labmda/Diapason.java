f5baad8b07c220d2ffa8ebbd4080485521f3f481
Aleksey Prozorov <kejamwork@mail.ru>
2018-12-04 15:32:24 +0300

    test

--- chapter_004/src/main/java/ru/job4j/labmda/Diapason.java
+++ chapter_004/src/main/java/ru/job4j/labmda/Diapason.java
@@ -8,9 +8,15 @@ public class Diapason {
     List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new LinkedList<>();
         while (start != end) {
-            
+            result.add(....);
             start++;
         }
+        result.forEach(d->System.out.println(d));
         return result;
+    }
+
+    public static void main(String[] args) {
+        Diapason diapason = new Diapason();
+        diapason.diapason(1,3, d -> d * d);
     }
 }
