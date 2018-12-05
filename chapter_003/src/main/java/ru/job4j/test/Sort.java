6c05dfe78752e0caaaddda8a59a2ccaa3f98e9a1
Aleksey Prozorov <kejamwork@mail.ru>
2018-12-05 14:16:39 +0300

    Отсортировать департаменты

---
+++ chapter_003/src/main/java/ru/job4j/test/Sort.java
@@ -0,0 +1,79 @@
+package ru.job4j.test;
+/**
+ * kejam.
+ */
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ *  Class for sorter String.
+ */
+public class Sort {
+        /**
+         *  List sort.
+         */
+    private ArrayList<String> sort = new ArrayList<>();
+
+    /**
+     *  Add element into collection.
+     * @param name
+     * @return
+     */
+    public boolean add(String name) {
+        boolean result = false;
+        if (name != null) {
+            sort.add(name);
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * String convert in array of char and equals by value.
+     * @param
+     * @return
+     */
+    public boolean sorter(int i) {
+        boolean result = false;
+        sort.sort(
+                new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        char[] cL = o1.toCharArray();
+                        char[] cR = o2.toCharArray();
+                        int min = Math.min(cL.length, cR.length);
+                        int result = 0;
+                           if (i == 0) {
+                               for (int i = 0; i < min; i++) {
+                                   result = Character.compare(cL[i], cR[i]);
+                                   if (result != 0) {
+                                       break;
+                                   }
+                               }
+                               return result != 0 ? result : Integer.compare(o1.length(), o2.length());
+                           }
+                           if (i == 1) {
+                               for (int i = 0; i < min; i++) {
+                                   result = Character.compare(cR[i], cL[i]);
+                                   if (result != 0) {
+                                       break;
+                                   }
+                               }
+                               return result != 0 ? result : Integer.compare(o1.length(), o2.length());
+                           }
+                           return result;
+                    }
+                }
+            );
+        return result;
+    }
+
+    /**
+     * Return all array.
+     * @return
+     */
+    public ArrayList<String> getSort() {
+        return sort;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/test/SortTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortTest {
+    @Test
+    public void whenSortUp() {
+        Sort sort = new Sort();
+        sort.add("K1\\SK1");
+        sort.add("K2\\SK1");
+        sort.add("K1\\SK1\\SSK2");
+        sort.add("K1\\SK1\\SSK1");
+        sort.add("K2");
+        sort.add("K1");
+        sort.add("K2\\SK1\\SSK1");
+        sort.add("K2\\SK1\\SSK2");
+        sort.sorter(0);
+        ArrayList<String> actual = sort.getSort();
+        List<String> expect =  Arrays.asList(
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        );
+        assertThat(actual, is(expect));
+    }
+    @Test
+    public void whenSortDown() {
+        Sort sort = new Sort();
+        sort.add("K1\\SK1");
+        sort.add("K2\\SK1");
+        sort.add("K1\\SK1\\SSK2");
+        sort.add("K1\\SK1\\SSK1");
+        sort.add("K2");
+        sort.add("K1");
+        sort.add("K2\\SK1\\SSK1");
+        sort.add("K2\\SK1\\SSK2");
+        sort.sorter(1);
+        ArrayList<String> actual = sort.getSort();
+        List<String> expect =  Arrays.asList(
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1"
+        );
+        assertThat(actual, is(expect));
+    }
+}
