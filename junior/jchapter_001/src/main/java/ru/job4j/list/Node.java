7696c459d1ab00c4b0b2a05e86139bd79a91edfb
kejam <kejamwork@mail.ru>
2018-12-11 10:57:57 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#96601]

---
+++ junior/jchapter_001/src/main/java/ru/job4j/list/Node.java
@@ -0,0 +1,16 @@
+package ru.job4j.list;
+
+class Node<T> {
+    public Node(T value) {
+        this.value = value;
+    }
+
+    T value;
+    Node<T> next;
+
+    boolean hasCycle(Node first) {
+        boolean result = false;
+        //
+        return result;
+    }
+}
---
+++ junior/jchapter_001/src/test/java/ru/job4j/list/NodeTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NodeTest {
+    @Test
+    public void when() {
+        Node first = new Node(1);
+        Node two = new Node(2);
+        Node third = new Node(3);
+        Node four = new Node(4);
+        assertThat(first.hasCycle(two), is(true));
+    }
+}
