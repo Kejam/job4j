1f252bd235a3f76d3a5f782d66648d9be9eb5543
kejam <kejamwork@mail.ru>
2018-12-12 07:13:57 +0300

    2. Не перекрывать equals hashCode[#96620]

---
+++ junior/jchapter_001/src/test/java/ru/job4j/map/MapTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    @Test
+    public void whenCreateTwoUserWithoutHashcode() {
+        User user = new User("Alex", 21, new GregorianCalendar(2000, 12, 12));
+        User user1 = new User("Alex", 21, new GregorianCalendar(2000, 12, 12));
+        Map<User, String> map = new HashMap<>();
+        map.put(user, "hoho");
+        map.put(user1, "hehe");
+        System.out.println(map);
+    }
+}
