ff0f73a20a5f0d1c203fe56201df2249987a7fcc
kejam <kejamwork@mail.ru>
2018-12-12 07:05:58 +0300

    1. Создать модель User[#96616]

---
+++ junior/jchapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,18 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+/**
+ * @author Aleksey Prozorov (kejamwork@mail.ru)
+ * This class implements user.
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
