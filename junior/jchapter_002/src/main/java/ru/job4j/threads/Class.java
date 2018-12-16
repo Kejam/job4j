695937845a3102188c198bb40a13d751655a3870
kejam <kejamwork@mail.ru>
2018-12-16 20:18:33 +0300

    1. Проиллюстрировать проблемы с многопоточностью.[#96680]

---
+++ junior/jchapter_002/src/main/java/ru/job4j/threads/Class.java
@@ -0,0 +1,19 @@
+package ru.job4j.threads;
+
+import java.util.Random;
+
+
+
+public class Class {
+    public int[] ar = new int[10];
+    public Random random = new Random();
+    public int index = 0;
+
+    public static void main(String[] args) {
+        thread1.start;
+        thread2.start;
+        for(int i: ar) {
+            System.out.println(i);
+        }
+    }
+}
