736ac8196e20dbc328b8c8e7bbc635a9a0e7cf35
kejam <kejamwork@mail.ru>
2019-01-04 15:06:16 +0300

    1. Реализовать приложения для работы с пользователем.[#96651]

---
+++ junior/jchapter_004/src/main/java/ru/job4j/servlets/UserCreateServlet.java
@@ -0,0 +1,61 @@
+package ru.job4j.servlets;
+
+import ru.job4j.logic.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserCreateServlet extends HttpServlet {
+    private final MemoryStore storage = MemoryStore.getInstance();
+    private String getHtml(String message, String method, String form) {
+        return "<!DOCTYPE html>"
+                + "<html lang=\"en\">"
+                + "<head>"
+                + "    <meta charset=\"UTF-8\">"
+                + "    <title>Create User</title>"
+                + "</head>"
+                + "<body>"
+                + message
+                + method
+                + form
+                + "</body>"
+                + "</html>";
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        var html = this.getHtml(
+                "",
+                "<form action='" + req.getContextPath() + "/create' method='post'>"
+                        + "Id : <input type='text' name='id'/>"
+                        + "Name : <input type='text' name='name'/>"
+                        + "Login : <input type='text' name='login'/>"
+                        + "Email : <input type='text' name='email'/>"
+                        + "<input type='submit' value='create'>"
+                        + "</form>",
+                ""
+        );
+        writer.append(html);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            storage.add(
+                    new User(
+                            Integer.parseInt(req.getParameter("id")),
+                            req.getParameter("name"),
+                            req.getParameter("login"),
+                            req.getParameter("email")
+                    )
+            );
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ junior/jchapter_004/src/main/java/ru/job4j/servlets/UsersServlet.java
@@ -0,0 +1,25 @@
+package ru.job4j.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UsersServlet extends HttpServlet {
+    private final MemoryStore storage = MemoryStore.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final PrintWriter writer = resp.getWriter();
+        writer.append("<html>");
+        writer.append("<body>List users</body>");
+        writer.append("<body>");
+        this.storage
+                .findAll()
+                .forEach(writer::println);
+        writer.append("</body>");
+        writer.append("</html>");
+        writer.flush();
+    }
+}
