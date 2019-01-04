620463cfc74a1c69e4bad5ded023ce4c4bf24463
kejam <kejamwork@mail.ru>
2019-01-04 16:29:24 +0300

    1. Реализовать приложения для работы с пользователем.[#96651]

--- junior/jchapter_004/src/main/java/ru/job4j/servlets/UserUpdateServlet.java
+++ junior/jchapter_004/src/main/java/ru/job4j/servlets/UserUpdateServlet.java
@@ -1,19 +1,57 @@
 package ru.job4j.servlets;
 
+import ru.job4j.logic.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class UserUpdateServlet extends HttpServlet {
+    private final MemoryStore storage = MemoryStore.getInstance();
+    private String getHtml(String message, String method, String form) {
+        return "<!DOCTYPE html>"
+                + "<html lang=\"en\">"
+                + "<head>"
+                + "    <meta charset=\"UTF-8\">"
+                + "    <title>Update User</title>"
+                + "</head>"
+                + "<body>"
+                + message
+                + method
+                + form
+                + "</body>"
+                + "</html>";
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        final PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        int id = Integer.parseInt(req.getParameter("id")) - 1;
+        User user = storage.findById(id);
+        var html = this.getHtml(
+                "",
+                "<form action='" + req.getContextPath() + "/list' method='post'>"
+                        + "Id : <input type='text' value='" + user.getId() + "' name='id'/>"
+                        + "Name : <input type='text' value='" + user.getName() + "' name='name'/>"
+                        + "<input type='submit' value='update'>"
+                        + "</form>",
+                ""
+        );
+        writer.append(html);
+        writer.flush();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        try {
+            storage.update(
+                    Integer.parseInt(req.getParameter("id")),
+                    req.getParameter("name")
+            );
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
