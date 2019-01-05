d04c4b287d8e065bc6e768ddaf52a3de5a5e2791
kejam <kejamwork@mail.ru>
2019-01-05 16:59:22 +0300

    1. Реализовать ThreadPool[#96669]

--- junior/jchapter_002/src/main/java/ru/job4j/poll/ThreadPool.java
+++ junior/jchapter_002/src/main/java/ru/job4j/poll/ThreadPool.java
@@ -1,5 +1,6 @@
 package ru.job4j.poll;
 
+import javafx.concurrent.Worker;
 import ru.job4j.wait.SimpleBlockingQueue;
 
 import java.util.LinkedList;
@@ -8,19 +9,46 @@ import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadPool {
-    private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
-    private int size = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads;
+    private final SimpleBlockingQueue<Runnable> tasks;
+    private int size;
+
+    public ThreadPool(int size) {
+        this.size = Runtime.getRuntime().availableProcessors();
+        this.threads = new LinkedList<>();
+        this.tasks = new SimpleBlockingQueue<>(100);
+        for (int i = 0; i < size; i++) {
+            this.threads.add(new Worker(this.tasks));
+        }
+        this.threads.forEach(Thread::start);
+    }
 
     public void work(Runnable job) throws InterruptedException {
-        if (tasks.getSize() < 1) {
-            job.wait();
-        }
-        tasks.offer(job);
+        this.tasks.offer(job);
     }
 
     public void shutdown() throws InterruptedException {
+        threads.forEach(Thread::interrupt);
+    }
 
+    private class Worker extends Thread {
+
+        private final SimpleBlockingQueue<Runnable> tasks;
+
+        private Worker(final SimpleBlockingQueue<Runnable> tasks) {
+            this.tasks = tasks;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    tasks.poll().run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 
 }
