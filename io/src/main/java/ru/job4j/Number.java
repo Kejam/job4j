8a4f9bd8a4d4b7d368053f710229e359a547fab4
Kejam <kejamwork@mail.ru>
2019-01-22 13:22:41 +0300

    Проверить байтовый поток

---
+++ io/src/main/java/ru/job4j/Number.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class Number implements AutoCloseable {
+    public boolean isNumber(InputStream in) {
+        boolean result = false;
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        ) {
+            String value = br.lines().collect(Collectors.joining("\n"));
+            if (Integer.parseInt(value) % 2 == 0) {
+                result = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
---
+++ io/src/test/java/ru/job4j/NumberTest.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class NumberTest {
+    private Number number;
+
+    @Before
+    public void setUp() {
+        number = new Number();
+    }
+
+    @Test
+    public void whenNumberIsEven() {
+        InputStream in = new ByteArrayInputStream(StandardCharsets.UTF_16.encode("12").array());
+        assertThat(number.isNumber(in), is(true));
+    }
+    @Test
+    public void whenNumberIsoOdd() {
+        InputStream in = new ByteArrayInputStream(StandardCharsets.UTF_16.encode("11").array());
+        assertThat(number.isNumber(in), is(true));
+    }
+    @Test
+    public void whenCatchException() {
+        InputStream in = new ByteArrayInputStream(StandardCharsets.UTF_16.encode("11").array());
+        assertThat(number.isNumber(in), is("NumberFormatException"));
+    }
+}
